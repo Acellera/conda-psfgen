afa345100426979c28ac111d629f0fc0a125327e
M J Harvey <m.j.harvey@acellera.com>
2016-03-11 20:08:52 +0100

    build rev

--- package/psfgen/build.sh
+++ package/psfgen/build.sh
@@ -4,4 +4,5 @@ if [ "$CC" == "" ]; then CC=gcc; fi
 make CC=$CC
 cp psfgen "$PREFIX/bin/psfgen.bin"
 echo 'DIR=$(dirname "$(which python)"); LD_LIBRARY_PATH="$DIR/.lib/compat-libc" "$DIR/psfgen.bin" $*' > "$PREFIX/bin/psfgen"
+chmod +x "$PREFIX/bin/psfgen"
 
