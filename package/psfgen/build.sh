ec58bdf8bb559984323a9211d665acec049dccad
M J Harvey <m.j.harvey@acellera.com>
2016-03-15 18:12:26 +0100

    first cut at cross-compile

--- package/psfgen/build.sh
+++ package/psfgen/build.sh
@@ -1,8 +1,15 @@
-if [ "$CC" == "" ]; then CC=gcc; fi
+if [ "$CC" == "" ] ; then CC=gcc-4.4;  fi
+if [ "$CXX" == "" ]; then CXX=g++-4.4; fi
 
+CFLAGS=""
 
-make CC=$CC
-cp psfgen "$PREFIX/bin/psfgen.bin"
-echo 'DIR=$(dirname "$(which python)"); LD_LIBRARY_PATH="$DIR/../lib/compat-libc" "$DIR/psfgen.bin" $@' > "$PREFIX/bin/psfgen"
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+ CFLAGS="--static"
+fi
+
+make "CC=$CC" "CXX=$CXX" "CFLAGS=$CFLAGS"
+mkdir -p "$PREFIX/bin"
+cp psfgen "$PREFIX/bin/psfgen"
+#echo 'DIR=$(dirname "$(which python)"); LD_LIBRARY_PATH="$DIR/../lib/compat-libc" "$DIR/psfgen.bin" $@' > "$PREFIX/bin/psfgen"
 chmod +x "$PREFIX/bin/psfgen"
 
