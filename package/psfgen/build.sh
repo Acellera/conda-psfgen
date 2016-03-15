eee3bc057da68841a251c961ef6540ae8bdc64c0
M J Harvey <m.j.harvey@acellera.com>
2016-03-15 20:37:14 +0100

    diddle

--- package/psfgen/build.sh
+++ package/psfgen/build.sh
@@ -2,14 +2,23 @@ if [ "$CC" == "" ] ; then CC=gcc-4.4;  fi
 if [ "$CXX" == "" ]; then CXX=g++-4.4; fi
 
 CFLAGS=""
-
+LIBS=""
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
  CFLAGS="--static"
+ LIBS="-ltcl"
+fi
+if [ "$CROSS_COMPILE" == "1" ]; then
+ CC=x86_64-w64-mingw32-gcc
+ LIBS="-Llib -ltcl86"
 fi
 
-make "CC=$CC" "CXX=$CXX" "CFLAGS=$CFLAGS"
+make "CC=$CC" "CXX=$CXX" "CFLAGS=$CFLAGS" "LIBS=$LIBS"
 mkdir -p "$PREFIX/bin"
 cp psfgen "$PREFIX/bin/psfgen"
-#echo 'DIR=$(dirname "$(which python)"); LD_LIBRARY_PATH="$DIR/../lib/compat-libc" "$DIR/psfgen.bin" $@' > "$PREFIX/bin/psfgen"
+
+if [ "$CROSS_COMPILE" == "1" ]; then
+ cp lib/*dll "$PREFIX/bin"
+fi
+
 chmod +x "$PREFIX/bin/psfgen"
 
