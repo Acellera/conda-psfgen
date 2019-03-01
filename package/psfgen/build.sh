c24802a6738b01dcc508d8e69d5523b890777f96
sdoerr <stefan.doerr@upf.edu>
2019-03-01 12:19:49 +0100

    added tcl libraries and includes

--- package/psfgen/build.sh
+++ package/psfgen/build.sh
@@ -2,23 +2,23 @@ if [ "$CC" == "" ] ; then CC=gcc-4.4;  fi
 if [ "$CXX" == "" ]; then CXX=g++-4.4; fi
 
 CFLAGS=""
-LIBS="-ltcl"
+LIBS="-ltcl8.6"
 
 if [ "$TRAVIS_OS_NAME" == "<UNDEFINED>" ]; then
  CC=gcc
- LIBS="-L$PREFIX/lib -ltcl"
+ LIBS="-L$PREFIX/lib -ltcl8.6"
 fi
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
  CFLAGS="--static -O3 -fexpensive-optimizations -ffast-math"
- LIBS="-ltcl -lpthread -ldl -lrt -lm"
+ LIBS="-ltcl8.6 -lpthread -ldl -lrt -lm"
 fi
 if [ "$CROSS_COMPILE" == "1" ]; then
  CC=x86_64-w64-mingw32-gcc
- LIBS="-Llib -ltcl86"
+ LIBS="-Llib -ltcl8.6"
 fi
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
- LIBS="-ltcl"
+ LIBS="-ltcl8.6"
 fi
 
 make "CC=$CC" "CXX=$CXX" "CFLAGS=$CFLAGS" "LIBS=$LIBS"
