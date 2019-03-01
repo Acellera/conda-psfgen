80f1ae926409a2c74d9931cc945a4d86b8bc671d
Stefan Doerr <stefan.doerr@upf.edu>
2019-03-01 17:21:05 +0100

    fixed typo

---
+++ makerelease.py
@@ -0,0 +1,19 @@
+# (c) 2015-2018 Acellera Ltd http://www.acellera.com
+# All Rights Reserved
+# Distributed under HTMD Software License Agreement
+# No redistribution in whole or part
+#
+from subprocess import call
+
+
+def makeMajorRelease(version, message, testfolder):
+    relname = 'rel-{}'.format(version)
+    call(['git', 'checkout', 'master'])
+    call(['git', 'fetch'])
+    call(['git', 'pull'])
+    call(['git', 'checkout', '-b', relname])
+    call(['git', 'tag', version, '-m', message])
+    call(['git', 'push', '--tags', 'origin', relname])
+    call(['git', 'checkout', 'master'])
+    call(['git', 'tag', version, '-m', message])
+    call(['git', 'push', '--tags'])
